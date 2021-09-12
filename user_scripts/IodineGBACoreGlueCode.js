40746566db1a0707fade324e2b723925e3c9d426
Browncha023 <90405291+Browncha023@users.noreply.github.com>
2021-09-11 22:59:24 -0600

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -20,8 +20,6 @@ var games = {
     "advancewars2":"Advance Wars 2",
     "aladdin":"Aladdin",
     "alienhominid":"Alien Hominid",
-    "bkgr":"Banjo Kazooie - Grunty's Revenge",
-    "bp":"Banjo Pilot",
     "bomberman_max2blue":"Bomberman Max 2 - Blue Advance",
     "bomberman_tournament":"Bomberman Tournament",
     "bubblebobble":"Bubble Bobble",

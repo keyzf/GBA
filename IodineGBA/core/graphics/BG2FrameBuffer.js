6e4457a2b5ba6bc0e56324bddf070517305e2b0c
Browncha023 <90405291+Browncha023@users.noreply.github.com>
2021-09-13 16:28:43 -0600

    Update BG2FrameBuffer.js

--- IodineGBA/core/graphics/BG2FrameBuffer.js
+++ IodineGBA/core/graphics/BG2FrameBuffer.js
@@ -1,11 +1,8 @@
 "use strict";
 /*
- Copyright (C) 2012-2015 Grant Galitz
- 
+ Copyright (C) 2012-2016 Grant Galitz
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 function GameBoyAdvanceBG2FrameBufferRenderer(gfx) {
@@ -16,7 +13,6 @@ GameBoyAdvanceBG2FrameBufferRenderer.prototype.initialize = function () {
     this.VRAM = this.gfx.VRAM;
     this.VRAM16 = this.gfx.VRAM16;
     this.fetchPixel = this.fetchMode3Pixel;
-    this.bgAffineRenderer = this.gfx.bgAffineRenderer0;
     this.frameSelect = 0;
 }
 GameBoyAdvanceBG2FrameBufferRenderer.prototype.selectMode = function (mode) {
@@ -32,10 +28,6 @@ GameBoyAdvanceBG2FrameBufferRenderer.prototype.selectMode = function (mode) {
             this.fetchPixel = this.fetchMode5Pixel;
     }
 }
-GameBoyAdvanceBG2FrameBufferRenderer.prototype.renderScanLine = function (line) {
-    line = line | 0;
-    this.bgAffineRenderer.renderScanLine(line | 0, this);
-}
 if (__LITTLE_ENDIAN__) {
     if (typeof Math.imul == "function") {
         //Math.imul found, insert the optimized path in:
@@ -43,7 +35,7 @@ if (__LITTLE_ENDIAN__) {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 240 && (y | 0) < 160) {
+            if ((x >>> 0) < 240 && (y >>> 0) < 160) {
                 var address = (Math.imul(y | 0, 240) + (x | 0)) | 0;
                 return this.VRAM16[address & 0xFFFF] & 0x7FFF;
             }
@@ -54,7 +46,7 @@ if (__LITTLE_ENDIAN__) {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 160 && (y | 0) < 128) {
+            if ((x >>> 0) < 160 && (y >>> 0) < 128) {
                 var address = ((this.frameSelect | 0) + Math.imul(y | 0, 160) + (x | 0)) | 0;
                 return this.VRAM16[address & 0xFFFF] & 0x7FFF;
             }
@@ -68,7 +60,7 @@ if (__LITTLE_ENDIAN__) {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 240 && (y | 0) < 160) {
+            if ((x >>> 0) < 240 && (y >>> 0) < 160) {
                 var address = (((y * 240) | 0) + (x | 0)) | 0;
                 return this.VRAM16[address & 0xFFFF] & 0x7FFF;
             }
@@ -79,7 +71,7 @@ if (__LITTLE_ENDIAN__) {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 160 && (y | 0) < 128) {
+            if ((x >>> 0) < 160 && (y >>> 0) < 128) {
                 var address = ((this.frameSelect | 0) + ((y * 160) | 0) + (x | 0)) | 0;
                 return this.VRAM16[address & 0xFFFF] & 0x7FFF;
             }
@@ -95,7 +87,7 @@ else {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 240 && (y | 0) < 160) {
+            if ((x >>> 0) < 240 && (y >>> 0) < 160) {
                 var address = (Math.imul(y | 0, 240) + (x | 0)) << 1;
                 return ((this.VRAM[address | 1] << 8) | this.VRAM[address | 0]) & 0x7FFF;
             }
@@ -106,7 +98,7 @@ else {
             x = x | 0;
             y = y | 0;
             //Output pixel:
-            if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 160 && (y | 0) < 128) {
+            if ((x >>> 0) < 160 && (y >>> 0) < 128) {
                 var address = ((this.frameSelect | 0) + ((Math.imul(y | 0, 160) + (x | 0)) << 1)) | 0;
                 return ((this.VRAM[address | 1] << 8) | this.VRAM[address | 0]) & 0x7FFF;
             }
@@ -118,7 +110,7 @@ else {
         //Math.imul not found, use the compatibility method:
         GameBoyAdvanceBG2FrameBufferRenderer.prototype.fetchMode3Pixel = function (x, y) {
             //Output pixel:
-            if (x > -1 && y > -1 && x < 240 && y < 160) {
+            if ((x >>> 0) < 240 && (y >>> 0) < 160) {
                 var address = ((y * 240) + x) << 1;
                 return ((this.VRAM[address | 1] << 8) | this.VRAM[address]) & 0x7FFF;
             }
@@ -127,7 +119,7 @@ else {
         }
         GameBoyAdvanceBG2FrameBufferRenderer.prototype.fetchMode5Pixel = function (x, y) {
             //Output pixel:
-            if (x > -1 && y > -1 && x < 160 && y < 128) {
+            if ((x >>> 0) < 160 && (y >>> 0) < 128) {
                 var address = this.frameSelect + (((y * 160) + x) << 1);
                 return ((this.VRAM[address | 1] << 8) | this.VRAM[address]) & 0x7FFF;
             }
@@ -142,7 +134,7 @@ if (typeof Math.imul == "function") {
         x = x | 0;
         y = y | 0;
         //Output pixel:
-        if ((x | 0) > -1 && (y | 0) > -1 && (x | 0) < 240 && (y | 0) < 160) {
+        if ((x >>> 0) < 240 && (y >>> 0) < 160) {
             var address = ((this.frameSelect | 0) + (Math.imul(y | 0, 240) | 0) + (x | 0)) | 0;
             return this.palette[this.VRAM[address | 0] & 0xFF] | 0;
         }
@@ -154,7 +146,7 @@ else {
     //Math.imul not found, use the compatibility method:
     GameBoyAdvanceBG2FrameBufferRenderer.prototype.fetchMode4Pixel = function (x, y) {
         //Output pixel:
-        if (x > -1 && y > -1 && x < 240 && y < 160) {
+        if ((x >>> 0) < 240 && (y >>> 0) < 160) {
             return this.palette[this.VRAM[this.frameSelect + (y * 240) + x]];
         }
         //Out of range, output transparency:
